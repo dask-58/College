91ce11113cda3bd260e3992b080ae50fb4741e71
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2023-08-31 15:16:33 +0530

    Fibonacci number
    Takes input i and prints the i th fibonacci number.

---
+++ fibonnacci.cpp
@@ -0,0 +1,24 @@
+//codeby proitdtdelre
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+int fib(int n){
+    if(n==1){
+        return 0;
+    }
+    if(n==2){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+
+}
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin >> t;
+cout << fib(t) << "\n";
+return 0;
+}
