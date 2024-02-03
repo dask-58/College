69363a57e172f96464a3cbaa01195c2cd848afcd
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-02-03 18:03:39 +0530

    added visual graphs

---
+++ Python/somegraphs.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.xlabel('X values')
+plt.ylabel('Y values')
+
+plt.title('Graph hahaha')
+
+plt.grid(True)
+
+x = [1, 2, 3, 4, 5]
+y = [12, 25, 109, 11, 3]
+
+plt.plot(x, y)
+
+
+plt.show()
