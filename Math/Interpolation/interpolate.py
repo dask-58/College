7ee554590a6d59a09532aaba49fee5e4d60c11f5
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-03-04 16:57:58 +0530

    Add : some problems on ll

--- Math/Interpolation/interpolate.py
+++ Math/Interpolation/interpolate.py
@@ -4,7 +4,7 @@ def interpolate(d, x):
             ans = d[i][1] + (x - d[i][0]) * ((d[i + 1][1] - d[i][1]) / (d[i + 1][0] - d[i][0]))
             return ans
 
-data = [[2026, 23],  [2028, 24.24]]
+data = [[2023, 10],  [2024, 10.5], [2025, 9], [2026, 12], [2028, 12.5]]
 year = 2027
 
 print('The estimated value for x value',year, 'with around 99.867% accuracy is', interpolate(data, year))
